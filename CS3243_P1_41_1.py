4c7d7ef454c40155ee1d3c5a45fa1d0922fbc01b
Rohan Arya Varma <rohanaryavarma@gmail.com>
2020-02-18 14:40:30 +0800

    Add Manhattan Distance heuristic (h1); clear CS3243_P1_41_1.py

--- CS3243_P1_41_1.py
+++ CS3243_P1_41_1.py
@@ -3,65 +3,22 @@
 import os
 import sys
 import math
-import heapq
 from time import time # assumes Unix-based system; switch to clock if on Windows
 
 class Node(object):
     def __init__(self, orientation):
         self.state = orientation # a list of lists corresponding to the orientation of the tiles
-        self.current_cost = float('inf')
-        self.heuristic_value = float('inf')
-        self.previous_action = "START"
-        self.previous_node = None
-        
-        # stats
-        self.depth = float('inf')
+        self.depth = 0
     
-    def __lt__(self, other):
-        return (self.current_cost + self.heuristic_value) < (other.current_cost + other.heuristic_value)
-
-    def __eq__(self, other):
-        return (self.current_cost + self.heuristic_value) == (other.current_cost + other.heuristic_value)
-
-    # set current path cost
-    def set_current_cost(self, cost):
-        self.current_cost = cost
-
-    # get current path cost
-    def get_current_cost(self):
-        return self.current_cost
-
-    # set heuristic value
-    def set_heuristic_value(self, value):
-        self.heuristic_value = value
-    
-    # get heuristic value
-    def get_heuristic_value(self):
-        return self.heuristic_value
-
-    # set current depth
-    def set_current_depth(self, depth):
-        self.depth = depth
-
-    def set_previous_action(self, action):
-        self.previous_action = action
-
-    def get_previous_action(self):
-        return self.previous_action
-
-    def set_previous_node(self, node):
-        self.previous_node = node
-    
-    def get_previous_node(self):
-        return self.previous_node
+    # compare function to check whether two States have the same orientation of tiles
+    def compare(self, other):
+        return self.state == other.state
 
 class Puzzle(object):
     def __init__(self, init_state, goal_state):
         # you may add more attributes if you think is useful
-        self.size = len(init_state)
         self.init_state = Node(init_state)
         self.goal_state = Node(goal_state)
-        self.visited = set()
         self.actions = list()
         self.max_depth = 0 # max depth reached by tree/graph search
         self.nodes_expanded = 0 # number of nodes expanded
@@ -71,37 +28,8 @@ class Puzzle(object):
         start = time()
         #TODO
         # implement your search algorithm here
-
-        '''
-        A* with h0: number of displaced tiles
-        '''
-        self.init_state.set_current_cost(0)
-        self.init_state.set_current_depth(0)
-        self.init_state.set_heuristic_value(self.get_heuristic_value(self.init_state))
-
-        frontier = []
-        heapq.heappush(frontier, self.init_state)
-
-        while frontier[0].get_heuristic_value() > 0:
-            curr = heapq.heappop(frontier)
-            self.visited.add(self.tupify(curr.state))
-
-            for i in self.generate_possibilities(curr):
-                i.set_current_cost(curr.get_current_cost() + 1)
-                i.set_heuristic_value(self.get_heuristic_value(i))
-                heapq.heappush(frontier, i)
-
-            if len(frontier) == 0:
-                return ["IMPOSSIBLE"]
-
-        backtrack = frontier[0]
-        while backtrack != None:
-            self.actions.append(backtrack.get_previous_action())
-            backtrack = backtrack.get_previous_node()
-
-        self.actions.reverse()
         self.time_taken = time() - start
-        return self.actions
+        return ["LEFT", "RIGHT"] # sample output 
 
     # returns Effective Branching Factor
     def get_EFB(self):
@@ -111,85 +39,6 @@ class Puzzle(object):
     def get_nodes_expanded(self):
         return self.nodes_expanded
 
-    # calculates heuristic value for a given node
-    def get_heuristic_value(self, node):
-        count = 0
-
-        for i in range(self.size):
-            for j in range(self.size):
-                if node.state[i][j] != self.goal_state.state[i][j]:
-                    count += 1
-        return count
-
-    def generate_possibilities(self, node):
-        blank_x = -1
-        blank_y = -1
-        nodes = []
-
-        for y in range(self.size):
-            if blank_x != -1:
-                break
-
-            for x in range(self.size):
-                if node.state[y][x] == 0:
-                    blank_x = x
-                    blank_y = y
-                    break
-        
-        # move blank left
-        if blank_x > 0:
-            temp = self.twodimensional_copy(node.state)
-            temp[blank_y][blank_x] = temp[blank_y][blank_x - 1]
-            temp[blank_y][blank_x - 1] = 0
-            if self.tupify(temp) not in self.visited:
-                temp_node = Node(temp)
-                temp_node.set_previous_node(node)
-                temp_node.set_previous_action("LEFT")
-                nodes.append(temp_node)
-        
-        # move blank right
-        if blank_x < self.size - 1:
-            temp = self.twodimensional_copy(node.state)
-            temp[blank_y][blank_x] = temp[blank_y][blank_x + 1]
-            temp[blank_y][blank_x + 1] = 0
-            if self.tupify(temp) not in self.visited:
-                temp_node = Node(temp)
-                temp_node.set_previous_node(node)
-                temp_node.set_previous_action("RIGHT")
-                nodes.append(temp_node)
-        
-        # move blank up
-        if blank_y > 0:
-            temp = self.twodimensional_copy(node.state)
-            temp[blank_y][blank_x] = temp[blank_y - 1][blank_x]
-            temp[blank_y - 1][blank_x] = 0
-            if self.tupify(temp) not in self.visited:
-                temp_node = Node(temp)
-                temp_node.set_previous_node(node)
-                temp_node.set_previous_action("UP")
-                nodes.append(temp_node)
-        
-        # move blank down
-        if blank_y < self.size - 1:
-            temp = self.twodimensional_copy(node.state)
-            temp[blank_y][blank_x] = temp[blank_y + 1][blank_x]
-            temp[blank_y + 1][blank_x] = 0
-            if self.tupify(temp) not in self.visited:
-                temp_node = Node(temp)
-                temp_node.set_previous_node(node)
-                temp_node.set_previous_action("DOWN")
-                nodes.append(temp_node)
-
-        return nodes
-    
-    def tupify(self, stateList):
-        outer = []
-        for i in stateList:
-            outer.append(tuple(i))
-        return tuple(outer)
-
-    def twodimensional_copy(self, stateList):
-        return [i[:] for i in stateList]
     # you may add more functions if you think is useful
 
 if __name__ == "__main__":
