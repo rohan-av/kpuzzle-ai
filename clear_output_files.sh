e133c5c0af7ef2e4fbe5055bd62e26c2e172257b
Rohan Arya Varma <rohanaryavarma@gmail.com>
2020-02-26 17:04:51 +0800

    Include multiple output files to examine differences in output

---
+++ clear_output_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# script to delete the output in all the output files (as compared to deleting them manually)
+
+> output.txt
+> output2.txt
+> output3.txt
+> output4.txt
+
+echo "All output file content deleted!"
--- run_searches.sh
+++ run_searches.sh
@@ -11,18 +11,18 @@ echo "Skip uninformed search? 0 for no, 1 for yes: "
 read skip
 
 > output.txt
+> output2.txt
+> output3.txt
 
 if [ $skip -eq 0 ]; then
   echo -e "\nUninformed Search:"
   python CS3243_P1_41_1.py public_tests_p1/n_equals_${size}/input_${input}.txt output.txt
-  > output.txt
 fi
 
 echo -e "\nInformed Search (Manhattan):"
-python CS3243_P1_41_2.py public_tests_p1/n_equals_${size}/input_${input}.txt output.txt
-> output.txt
+python CS3243_P1_41_2.py public_tests_p1/n_equals_${size}/input_${input}.txt output2.txt
 
 echo -e "\nInformed Search (Manhattan + LC):"
-python CS3243_P1_41_3.py public_tests_p1/n_equals_${size}/input_${input}.txt output.txt
+python CS3243_P1_41_3.py public_tests_p1/n_equals_${size}/input_${input}.txt output3.txt
 
 # to be updated with the final heuristic
